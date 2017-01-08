6c67843667c073ebb6e5502153c4447d0d198dbc
alfi setyadi <alfisetyadi@gmail.com>
2017-01-08 21:00:15 +0700

    GET /coupon/validate/:code

--- src/api/coupon.js
+++ src/api/coupon.js
@@ -30,5 +30,31 @@ export default () => {
     });
   });
 
+  // GET /validate/:code validation coupon code
+  router.get('/validate/:code', (req, res) => {
+    db.Coupon.findOne({code: req.params.code}, (err, coupon) => {
+      if (err) {
+        res.status(500);
+        res.end();
+      } 
+      if (coupon == null) {
+        res.status(200);
+        res.json({ valid: false });
+      } else {
+        // is valid code? is expired? is available limit?
+        if (coupon.limit < 1){
+          res.status(200);
+          res.json({ valid: false, message: "coupon has exceeds the limit" });
+        } else if (new Date() <= new Date(coupon.validfrom) || new Date() >= new Date(coupon.validto)){
+          res.status(200);
+          res.json({ valid: false, message: "coupon has expired" });
+        } else {
+          res.status(200);
+          res.json({ valid: true, value: coupon.value, message: "coupon valid" });
+        }
+      }
+    });
+  });
+
   return router;
 };
